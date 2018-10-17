082b8143c04dcb1d7b2e570740c1a92f96d69fbd
Heritage Samuel F <herrytheeagle@gmail.com>
2018-10-17 13:44:25 +0100

    imprvd

--- special method_iteration.py
+++ special method_iteration.py
@@ -1,25 +1,41 @@
-class range_examp:
+# class range_examp:
+#
+#     def __init__(self, end, step=1):
+#         self.current = 0
+#         self.end = end
+#         self.step = step
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         if self.current >= self.end:
+#             raise StopIteration()
+#         else:
+#             return_val =self.current
+#             self.current += self.step
+#             return return_val
+#
+# x = range_examp(5)
+#
+# x.__next__()
+# next(x)
+#
+# for i in x:
+#     print(i)
+#     print(dir(x))
 
-    def __init__(self, end, step=1):
-        self.current = 0
-        self.end = end
-        self.step = step
 
-    def __iter__(self):
-        return self
+def range_gen(end):
+    current = 0
+    while current < end:
+        yield current
+        current += 1
 
-    def __next__(self):
-        if self.current >= self.end:
-            raise StopIteration()
-        else:
-            return_val =self.current
-            self.current += self.step
-            return return_val
-
-x = range_examp(5)
-
+# for i in range_gen(5):
+#     print(i)
+x = range_gen(10)
 x.__next__()
-next(x)
 
 for i in x:
     print(i)
