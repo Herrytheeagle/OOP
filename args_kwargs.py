eb3d83c0d11dc441c4cacf0617382947fec2172a
Heritage Samuel F <herrytheeagle@gmail.com>
2018-10-18 14:16:14 +0100

    args-content

---
+++ args_kwargs.py
@@ -0,0 +1,13 @@
+web_com = 'commercial website'
+web_org = 'organisational website'
+web_edu = 'educational website'
+
+site_title = 'websites'
+
+
+def blog_posts(title, **kwargs):
+    print(title)
+    for p_title, post in args:
+        print(p_title, post)
+
+web_types(site_title, web_com, web_org, web_edu )
