0479decccdcf6a14abc4ba2d7c477cdedea6021e
Heritage Samuel F <herrytheeagle@gmail.com>
2018-10-18 14:43:43 +0100

    graph-basis

--- args_kwargs.py
+++ args_kwargs.py
@@ -1,13 +1,27 @@
+#
+# site_title = 'websites'
+#
+#
+# def web_types(title, **kwargs):
+#     print(title)
+#     for p_title, post in kwargs.items():
+#         print(p_title, post)
+#
+# web_types(site_title,
+#          web_com = 'commercial website',
+#          web_org = 'organisational website',
+#          web_edu = 'educational website',)
 
-site_title = 'websites'
+import matplotlib.pyplot as plt
 
 
-def web_types(title, **kwargs):
-    print(title)
-    for p_title, post in kwargs.items():
-        print(p_title, post)
+def graph_operation(x, y):
+    print('function that graphs {}  and {}'.format(str(x), str(y)))
+    plt.plot(x, y)
+    plt.show()
 
-web_types(site_title,
-         web_com = 'commercial website',
-         web_org = 'organisational website',
-         web_edu = 'educational website',)
+x1 = [1, 2, 3]
+y1 = [2, 3, 1]
+
+graph_operation(x1, y1)
+
